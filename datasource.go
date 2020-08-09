72f37a974a45fe42f180469db18644549f1de863
henrygas <873565064@qq.com>
2020-08-09 16:40:41 +0800

    ignore null values

--- datasource.go
+++ datasource.go
@@ -10,7 +10,7 @@ import (
 //var datasourceName = "root:1234@tcp(127.0.0.1:3306)/ys?timeout=90s&charset=utf8mb4&collation=utf8mb4_general_ci"
 
 func testOpen() {
-	db, err :=  sql.Open("mysql", "root:1234@/ys")
+	db, err := sql.Open("mysql", "root:1234@/ys")
 	if err != nil {
 		log.Fatal("fail to open mysql driver, ", err)
 		return
@@ -77,13 +77,60 @@ func testInsertAndQuery() {
 	num = 1
 	err = stmtQuery.QueryRow(num).Scan(&squareNum)
 	if err != nil {
-		log.Fatalf(fmt.Sprintf("failed to SELECT square_num BY num=%d", num), err)
+		log.Fatal(fmt.Sprintf("failed to SELECT square_num BY num=%d", num), err)
 	} else {
 		log.Printf("succ to SELECT, num=%d, square_num=%d\n", num, squareNum)
 	}
 }
 
+func testIgnoreNullValues() {
+	db, err := sql.Open("mysql", "root:1234@/ys")
+	if err != nil {
+		log.Fatal("failed to open mysql", err)
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM square_num")
+	if err != nil {
+		log.Fatal("failed to SELECT * FROM square_num", err)
+		return
+	}
+
+	columns, err := rows.Columns()
+	if err != nil {
+		log.Fatal("failed to get Columns()", err)
+		return
+	}
+
+	values := make([]sql.RawBytes, len(columns))
+	scanArgs := make([]interface{}, len(values))
+	for i, value := range values {
+		scanArgs[i] = &value
+	}
+
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			log.Fatal("failed to rows.Scan")
+			return
+		}
+		var value string
+		for i, col := range values {
+			if col == nil {
+				value = "NULL"
+			} else {
+				value = string(value)
+			}
+			fmt.Println(columns[i], ":", value)
+		}
+		fmt.Println("==================================")
+	}
+
+}
+
 func main() {
 	//testOpen()
-	testInsertAndQuery()
+	//testInsertAndQuery()
+	testIgnoreNullValues()
 }
